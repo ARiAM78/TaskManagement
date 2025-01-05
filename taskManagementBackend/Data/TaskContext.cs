dc66c1e055b3968e7657fe786aa8702b2bfaa446
Aryam Alshahrani <109631629+ARiAM78@users.noreply.github.com>
2025-01-05 19:53:33 +0300

    TaskContext.cs

--- taskManagementBackend/Data/TaskContext.cs
+++ taskManagementBackend/Data/TaskContext.cs
@@ -1,8 +1,17 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 public class TaskContext : DbContext
 {
+    // Constructor to initialize the DbContext with the options passed from the Startup class
     public TaskContext(DbContextOptions<TaskContext> options) : base(options) { }
 
-    public DbSet<Task> Tasks { get; set; } // Tasks table
+    // DbSet representing the Tasks table in the database
+    public DbSet<Task> Tasks { get; set; }
+
+    // This method allows for additional configuration of the model
+    // such as setting relationships between tables, defining constraints, etc.
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder); // Call the base method
+    }
 }
